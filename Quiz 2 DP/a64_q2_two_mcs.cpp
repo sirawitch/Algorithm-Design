262ef99e86d13c5107fc76584a539438a65d1699
sirawitch <57455984+sirawitch@users.noreply.github.com>
2022-03-20 10:17:56 +0700

    Update a64_q2_two_mcs.cpp

--- Quiz 2 DP/a64_q2_two_mcs.cpp
+++ Quiz 2 DP/a64_q2_two_mcs.cpp
@@ -3,26 +3,27 @@ using namespace std;
 int n,a=-5000,A[500001];
 long long dp[500002][2],mn=0,ans=0,sum=0;
 main()
-{ios_base::sync_with_stdio(false), cin.tie(NULL);
-cin>>n;
-for(int i=1;i<=n;++i){
-    cin>>A[i]; a=max(A[i],a);
-    sum+=A[i];
-    mn=min(mn,sum);
-    dp[i][0]=max(sum-mn,dp[i-1][0]);
-}
-sum=0;
-mn=0;
-for(int i=n;i>=1;--i){
-    sum+=A[i];
-    mn=min(mn,sum);
-    dp[i][1]=max(sum-mn,dp[i+1][1]);
-}
-for(int i=1;i<n;++i){
-    ans=max(dp[i][0]+dp[i+1][1],ans);
-}
-if(a<=0)cout<<a;
-else{cout<<ans;
-
-}
+{
+    ios_base::sync_with_stdio(false), cin.tie(NULL);
+    cin>>n;
+    for(int i=1;i<=n;++i){
+        cin>>A[i]; a=max(A[i],a);
+        sum+=A[i];
+        mn=min(mn,sum);
+        dp[i][0]=max(sum-mn,dp[i-1][0]);
+    }
+    sum=0;
+    mn=0;
+    for(int i=n;i>=1;--i){
+        sum+=A[i];
+        mn=min(mn,sum);
+        dp[i][1]=max(sum-mn,dp[i+1][1]);
+    }
+    for(int i=1;i<n;++i){
+        ans=max(dp[i][0]+dp[i+1][1],ans);
+    }
+    if(a<=0)cout<<a;
+    else{
+    cout<<ans;
+    }
 }
